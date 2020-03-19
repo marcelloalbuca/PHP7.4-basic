200436e2bbb92fbf96b510fe0933049ffd37ab30
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-03-19 19:33:55 -0300

    importe dos serviços e metodo

--- script.php
+++ script.php
@@ -1,6 +1,8 @@
 <?php
 
-session_start();
+include "servicos/servicoMsgSessao.php";
+include "servicos/servicoValicacao.php";
+include "servicos/servicoCatCompetidor.php";
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
@@ -8,3 +10,5 @@ $idade = $_POST['idade'];
 //var_dump($nome);
 //var_dump($idade);
 
+defineCatCompetidor($nome, $idade);
+header('location: index.php');
