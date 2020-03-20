e4e5f71ffd62c249cab3182fc9570daaae12ea1c
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-03-20 03:11:02 -0300

    conectando ao banco PDO

---
+++ pdo/connect.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+$pdo = null;
+
+try{
+    $pdo = new PDO('mysql:host=localhost;dbname=exemplo', 'root', 'root');
+} catch (Exception $e) {
+    echo $e->getMessage();
+    //die();
+}
+//var_dump($pdo);
+return $pdo;
